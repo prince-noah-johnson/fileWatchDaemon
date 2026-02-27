8da18f386988ff5a3f29e5be525448e9f127a8d3
Prince Noah Johnson <princenoha.j@gmail.com>
2026-02-27 00:26:21 -0500

    Inotify watch instance added

--- fileWatch.c
+++ fileWatch.c
@@ -11,13 +11,17 @@
 #define EXT_SUCCESS 0
 #define EXT_ERR_TOO_FEW_ARGS 1
 #define EXT_ERR_INIT_INOTIFY 2
+#define EXT_ERR_ADD_WATCH 3
 
 int IeventQueue = -1;
+int IeventStatus = -1;
 
 int main(int argc, char** argv) {
 
     char *basePath = NULL;
     char *token = NULL;
+
+    const uint32_t watchMask = IN_CREATE | IN_DELETE | IN_ACCESS | IN_CLOSE_WRITE | IN_MODIFY | IN_MOVE_SELF;
 
     if(argc < 2){
         fprintf(stderr, "USAGE: fileWatch PATH\n");
@@ -38,6 +42,12 @@ int main(int argc, char** argv) {
     if(IeventQueue == -1){
         fprintf(stderr, "Error initialising inotify instance\n");
         exit(EXT_ERR_INIT_INOTIFY);
+    }
+
+    IeventStatus = inotify_add_watch(IeventQueue, argv[1], watchMask);
+    if( IeventStatus == -1){
+        fprintf(stderr, "Error adding file to watch instance!\n");
+        exit(EXT_ERR_ADD_WATCH);
     }
 
     while (true) {
